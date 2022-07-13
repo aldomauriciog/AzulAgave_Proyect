bdaebae71030631d63c6b0b300a15f1916d3f6f5
Jahaziel <jahazielmauriciogonzales@gmail.com>
2022-07-13 03:34:10 -0500

    Upload Changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,3 +26,17 @@ function slides(){
         },1500)
     }
 }
+
+$(document).ready(function(){
+    $('a[href^="#"]').click(function(){
+        var destiny = $(this.hash);
+        if(destiny.length == 0){
+            destiny = $('a[name="' + this.hash.substr(1) + '"]');
+        }
+        if(destiny.length == 0){
+            destiny = $('html');
+        }
+        $('html, body').animate({ scrollTop: destiny.offset().top }, 500);
+        return false;
+    });
+});
