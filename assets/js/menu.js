97588e2d00d8b9fea3ec08684ea7e9b5e72c0bc7
Aldo Jahaziel Mauricio Gonzales <jahazielmauriciogonzales@gmail.com>
2022-07-14 19:10:28 -0500

    Upload Changes V.3

---
+++ assets/js/menu.js
@@ -0,0 +1,18 @@
+// $(document).ready(function(){
+//     $('a[href^="#"]').click(function(){
+//         var destiny = $(this.hash);
+//         if(destiny.length == 0){
+//             destiny = $('a[name="' + this.hash.substr(1) + '"]');
+//         }
+//         if(destiny.length == 0){
+//             destiny = $('html');
+//         }
+//         $('html, body').animate({ scrollTop: destiny.offset().top }, 500, );
+//         return false;
+//     });
+// });
+
+// $(document).ready(function(){
+//     $('a[href^="#sides"]').click(function(){
+//         $('html, body').animate({ scrollTop: $("#sides").offset().top }, 500, );});
+// });
